741e9cb7cd544880b942462506e92c2863434859
Long Zhen <michaelrobtemp@gmail.com>
2015-11-29 23:27:49 -0800

    start learning gof_dp & make cheat sheet

---
+++ Design_Pattern_cheatsheet.cs
@@ -0,0 +1,18 @@
+Creational Patterns
+** TODO understand **
+These design patterns provide a way to create objects while hiding the creation logic, 
+rather than instantiating objects directly using new opreator. 
+This gives program more flexibility in deciding which objects need to be created for a given use case.
+
+Structural Patterns
+** TODO understand **
+These design patterns concern class and object composition. 
+Concept of inheritance is used to compose interfaces and define ways to compose objects to obtain new functionalities.
+
+Behavioral Patterns
+** TODO understand **
+These design patterns are specifically concerned with communication between objects.
+
+TODO
+http://www.tutorialspoint.com/design_pattern/design_pattern_overview.htm
+http://zz563143188.iteye.com/blog/1847029
