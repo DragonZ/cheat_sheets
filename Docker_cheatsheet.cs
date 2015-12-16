51402c511bdf94f7bb30136fd445b8a142b272a7
DragonZ <michaelrobtemp@gmail.com>
2015-12-15 17:39:32 -0800

    new docker cheat sheet

---
+++ Docker_cheatsheet.cs
@@ -0,0 +1,18 @@
+COMMAND
+docker
+	- run <image:version> <command/application>
+		- -t ==> assign pseudo-tty/terminal
+		- -i ==> interactive connection
+		- -d ==> put background & daemonize
+		- -P ==> map container port to host
+		- -p <container-port> ==> exposed container port to host high port
+		- -p <host-port>:<container-port> ==> detail mapping
+	- ps ==> query container
+		- -l ==> details about last started container
+		- -a ==> all container including stopped
+	- logs <container-name> ==> container stdout
+	- stop <container-name> ==> stop container
+	- version ==> client & server/daemon info
+	- <subcommand> --help ==> all/subcommand help info
+	- port <container-id/name> <container-port> ==> port mapping info
+	
