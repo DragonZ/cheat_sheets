67a42851e62bde84f635baf44b00ea6994d64cf9
Long Zhen <lzhen@Longs-MacBook-Air.local>
2015-12-22 16:11:17 -0800

    add some docker machine tips

--- Docker_cheatsheet.cs
+++ Docker_cheatsheet.cs
@@ -19,4 +19,19 @@ docker
 SHORTCUT
 	- docker stop $(docker ps -a -q) ==> stop all containers
 	- docker rm $(docker ps -a -q) ==> remove all containers
-	
+
+DOCKER-MACHINE
+	- set up ssh keys
+	https://help.ubuntu.com/community/SSH/OpenSSH/Keys#Generating_RSA_Keys
+		- if virtual machine, may need host only network 
+		http://christophermaier.name/blog/2010/09/01/host-only-networking-with-virtualbox
+		- need sudo without password privilege
+		http://askubuntu.com/questions/147241/execute-sudo-without-password
+		https://github.com/docker/machine/issues/1569
+		- when use generic driver, better choose ubuntu as base OS @ this moment
+	- create the docker host
+	docker-machine create --driver generic \
+	--generic-ip-address 192.168.99.166 \
+	--generic-ssh-key /Users/lzhen/.ssh/id_rsa \
+	--generic-ssh-user lzhen \
+	dev
